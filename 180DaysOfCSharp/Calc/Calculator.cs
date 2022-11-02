372e0f347b240c725f5196791fdf7d7357cc6a6f
MACBOOK <macbook@MACBOOKs-MacBook-Pro.local>
2022-11-02 02:49:32 +0100

    Learning objects and namespaces

---
+++ 180DaysOfCSharp/Calc/Calculator.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace DaysOfCSharp.Calc
+{
+    public class Calculator
+    {
+        public static int Add(int x, int y)
+        {
+            int Answer = x + y;
+            return Answer;
+        }
+    }
+}
--- 180DaysOfCSharp/Program.cs
+++ 180DaysOfCSharp/Program.cs
@@ -1,12 +1,41 @@
 ﻿using System;
+using DaysOfCSharp.Calc;
 
 namespace DaysOfCSharp
 {
+    public class Person
+    {
+        public string Name;
+        public int Age;
+        public bool HasPet;
+
+        public int Calc(int x, int y)
+        {
+            int number = x + y;
+            return number;
+        }
+
+        public void Greeting()
+        {
+            Console.WriteLine("My name is " + Name + " and my age is " + Age);
+        }
+    }
+
     class MainClass
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Person person= new Person();
+
+            person.Name = "Benndip";
+            person.Age = 25;
+            person.HasPet = true;
+
+            person.Greeting();
+
+            int value = Calculator.Add(2, 3);
+
+            Console.WriteLine(value);
         }
     }
 }
